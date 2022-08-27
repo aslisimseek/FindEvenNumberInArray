70786b398e5054c2e8699b74c4041b2a96ccc779
Aslıhan Şimşek <simseekasli@gmail.com>
2022-08-27 15:57:28 +0300

    - Find Repeat Even Number in Array

--- FindEvenNumberInArray/src/FindEvenNumberInArray.java
+++ FindEvenNumberInArray/src/FindEvenNumberInArray.java
@@ -1,5 +1,19 @@
+import java.util.Arrays;
+
 public class FindEvenNumberInArray {
     public static void main(String[] args) {
-        
+        int[] list = {2, 3, 5, 3, 2, 4, 5, 6, 2, 3, 4,21};
+        int[] duplicateList = new int[list.length];
+        int startedIndex = 0;
+
+        for (int i = 0; i < list.length; i++) {
+            for (int j = 0; j < list.length; j++) {
+                if ((i != j) && list[i] == list[j] && list[i] % 2 == 0) {
+                    duplicateList[startedIndex++] = list[i];
+                    break;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(duplicateList));
     }
 }
